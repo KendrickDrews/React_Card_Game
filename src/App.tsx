9b1acd55c943211b597142be67330f0563a9d361
Kendrick Drews <drewskenny@gmail.com>
2024-07-12 19:04:55 -0400

    Layout Setup - 00 - 01

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
-import { useSelector, useDispatch } from 'react-redux'
-import { increment,decrement } from './redux/slices/counter/counterSlice'
-import { selectCount } from './redux'
+
 import './styles/main.scss'
+import { BackgroundLayer } from './Layers/00_Background'
+import { FightLayer } from './Layers/01_Fight'
 
 function App() {
-  // const [count, setCount] = useState(0)
-  const count = useSelector(selectCount)
-  const dispatch = useDispatch()
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-      <div>Count: {count}</div>
-        <button onClick={() => dispatch(increment())}>Increment</button>
-        <button onClick={() => dispatch(decrement())}>Decrement</button>
-      </div>
+      <FightLayer />
+      <BackgroundLayer />
     </>
   )
 }
--- src/Layers/01_Fight/FightLayer.tsx
+++ src/Layers/01_Fight/FightLayer.tsx
@@ -1,9 +1,35 @@
-import './App.css'
 
 const FightLayer = () => {
 
   return (
     <div className="layer-01-container">
+      <div className="run-info">
+        <div>
+          <div> character_Name</div>
+          <div> health</div>
+          <div> Gold</div>
+        </div>
+        <div> Current Floor </div>
+        <div> 
+          <div> Map </div>
+          <div> deck</div>
+          <div> controls</div>
+        </div>
+      </div>
+      <div className="artifact-bar"></div>
+      <div className="battle-stations"></div>
+      <div className="card-area">
+        <div className="draw-and-mana"></div>
+        <div className="deck">
+          <div className="card">01</div>
+          <div className="card">02</div>
+          <div className="card">03</div>
+          <div className="card">04</div>
+          <div className="card">05</div>
+        </div>
+        <div className="discard-and-endTurn"></div>
+      </div>
+      
       <h1>Layer 01 FIGHT</h1>
     </div>
   )
